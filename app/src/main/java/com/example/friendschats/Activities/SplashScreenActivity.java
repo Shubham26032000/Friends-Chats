f724dc6b137a893eba359c26720506b6b0aadb63
ADMIN <shubhamvast5@gmail.com>
2021-03-23 23:06:55 +0530

    Friends Chats App

---
+++ app/src/main/java/com/example/friendschats/Activities/SplashScreenActivity.java
@@ -0,0 +1,46 @@
+package com.example.friendschats.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.friendschats.databinding.ActivitySplashScreenBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ActivitySplashScreenBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        Intent intent;
+
+        if (auth != null)
+        {
+            intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        }else{
+            intent = new Intent(SplashScreenActivity.this, SignUpActivity.class);
+        }
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },1300);
+
+
+
+    }
+}
